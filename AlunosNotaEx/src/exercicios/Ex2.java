358edb1e28f49c46180d14f8fac7380655b1cb4c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-02-28 08:50:11 -0300

    Fix in CalcMedia and CalcAprovados

--- AlunosNotaEx/src/exercicios/Ex2.java
+++ AlunosNotaEx/src/exercicios/Ex2.java
@@ -4,66 +4,61 @@ import java.util.Scanner;
 
 public class Ex2 {
 
-		public static int ALUNOS = 30;
+	public static int ALUNOS = 30;
 
-		public static void main(String[] args) {
-			int rm[] = new int[ALUNOS];
-			double nota1[] = new double[ALUNOS];
-			double nota2[] = new double[ALUNOS];
-			
-			int n = entradaDados(rm,nota1,nota2);
-			double media[] = calculaMedia(nota1,nota2, n);
-			int aprovados[] = alunoAprovado(media,rm,n);
-			
-	 for(int i=0;i < n; i++){
-		 System.out.println("Media do aluno "+ rm[i] + " :" + media[i] + "\n");
-	 }
-	 System.out.println("RM dos alunos aprovados: ");
-	 for(int i = 0; i < n; i++){
-  	   System.out.println("RM: "+ aprovados[i]);
-     }	
+	public static void main(String[] args) {
+		int rm[] = new int[ALUNOS];
+		double nota1[] = new double[ALUNOS];
+		double nota2[] = new double[ALUNOS];
+		int aprovados[] = new int[ALUNOS];
+		double media[] = new double[ALUNOS];
+		int n = entradaDados(rm, nota1, nota2);
+		calculaMedia(nota1, nota2, n, media);
+		int NumAprovados = alunoAprovado(media, rm, n, aprovados);
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Media do aluno " + rm[i] + " :" + media[i] + "\n");
+		}
+		System.out.println("RM dos alunos aprovados: ");
+		for (int i = 0; i < NumAprovados; i++) {
+			System.out.println("RM: " + aprovados[i]);
+		}
+	}
+
+	public static int entradaDados(int rm[], double nota1[], double nota2[]) {
+		Scanner teclado = new Scanner(System.in);
+		int rmTemp = 0, n = 0;
+		while (rmTemp >= 0 && ALUNOS > n) {
+			System.out.println("Digite o RM do aluno " + (n + 1) + " :");
+			rmTemp = teclado.nextInt();
+			if (rmTemp < 0) {
+				break;
+			}
+			rm[n] = rmTemp;
+			System.out.println("Digite a nota1 do aluno " + (n + 1) + " :");
+			nota1[n] = teclado.nextDouble();
+			System.out.println("Digite o nota2 do aluno " + (n + 1) + " :");
+			nota2[n] = teclado.nextDouble();
+
+			n++;
+		}
+		teclado.close();
+		return n;
+	}
+	public static void calculaMedia(double[] nota1, double[] nota2, int n, double[] media) {
+		for (int i = 0; i < n; i++) {
+			media[i] = (nota1[i] + nota2[i]) / 2;
+		}
+	}
+	public static int alunoAprovado(double[] media, int[] rm, int n, int[] aprovados) {
+		int nAp = 0;
+		for (int i = 0; i < n; i++) {
+			if (media[i] >= 6) {
+				aprovados[nAp] = rm[i];
+				nAp++;
+			}
 		}
 
-	public static int entradaDados(int rm[], double nota1[], double nota2[]){
-		Scanner teclado = new Scanner(System.in);
-		int rmTemp = 0,index = 0;
-	       while(rmTemp >= 0 && ALUNOS > index){
-	    	     System.out.println("Digite o RM do aluno " + (index+1)+" :");
-	    	     rmTemp = teclado.nextInt();
-	    	     if(rmTemp < 0){
-	    	    	 break;
-	    	     }
-	    	     
-	    	     rm[index] = rmTemp;
-	    	     System.out.println("Digite a nota1 do aluno " + (index+1)+" :");
-	    	     nota1[index]  = teclado.nextDouble(); 	   
-	    	     System.out.println("Digite o nota2 do aluno " + (index+1) +" :");
-	    	     nota2[index]  = teclado.nextDouble();
-	    	     
-	    	  index++;
-	       }
-	      
-			teclado.close();
-		return index;
+		return nAp;
 	}
-	public static double[] calculaMedia(double[] nota1, double[] nota2, int index){
-		double media[] = new double[ALUNOS];
-		 for(int i=0; i < index; i++){
-	  	   media[i] = (nota1[i] + nota2[i]) / 2;
-	  	   }
-		return media;
-	}
-	public static int[] alunoAprovado(double[] media,int[] rm, int index){
-		int aprovados[] = new int[ALUNOS];
-		int nAp = 0;
-		
-		  for(int i=0; i < index; i++){
-	   	   if(media[i] >= 6){
-	 	    	 aprovados[nAp] = rm[i];
-	 	    	 nAp++;
-	   	   }
-	      }
-		  
-		return aprovados;
-}
 }
