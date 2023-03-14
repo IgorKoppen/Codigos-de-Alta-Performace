c9ba6936b1b4c540b8f8b78b7766df8eda703788
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-14 09:49:54 -0300

    DecToBin

--- PilhasSequenciais/src/pilhas/PilhaInt.java
+++ PilhasSequenciais/src/pilhas/PilhaInt.java
@@ -2,21 +2,50 @@ package pilhas;
 
 public class PilhaInt {
 
-	public final int N=6;
+	public final int N = 8;
 	int dados[] = new int[N];
 	int topo;
-	
-	
-	public void init(){
+
+	public void init() {
 		topo = 0;
 	}
-	public void push(int elem){
-	dados[topo] = elem;
-	topo++;
+
+	public void push(int elem) {
+		if (isFull())
+			System.out.println("Stack overflow");
+		else {
+			dados[topo] = elem;
+			topo++;
+		}
 	}
-	public int pop(){
+
+	public boolean isEmpty() {
+		if (topo == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean isFull() {
+		if (topo == N)
+			return true;
+		else
+			return false;
+	}
+
+	public int pop() {
 		topo--;
 		return (dados[topo]);
 	}
-	
+
+	public int top() {
+		return (dados[topo - 1]);
+	}
+
+	public void esvazia() {
+		while (!isEmpty())
+			System.out.println("  " + pop());
+		System.out.println();
+	}
+
 }
